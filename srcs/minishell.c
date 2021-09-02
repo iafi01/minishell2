2a9cb68f740e69444296628c68a51916b46ba92d
VigorNuminis <67346097+VigorNuminis@users.noreply.github.com>
2021-09-02 21:48:05 +0200

    implementazione lexer token

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -20,9 +20,53 @@ void    sign_handler(int sig)
 
 int	is_token(char *c)
 {
-	if (*c == '>' || *c == '<' || *c == '|' || ft_strncmp(c, ">>", 2) || ft_strncmp(c, "<<", 2))
+	if (*c+1 && (ft_strncmp(c, ">>", 2) || ft_strncmp(c, "<<", 2)))
+		return (2);
+	if (*c == '>' || *c == '<' || *c == '|')
 		return (1);
 	return (0);
+}
+
+t_token	*ft_token_new(t_type token, char *val)
+{
+	t_token	*lnew;
+
+	lnew = (t_token *)malloc(sizeof(t_token));
+	lnew->e_type = token;
+	lnew->val = val;
+	return (lnew);
+}
+
+t_token	*ft_find_end(t_token *list)
+{
+	if (!list)
+		return (NULL);
+	while (list->next != NULL)
+		list = list->next;
+	return (list);
+}
+
+void	ft_add_list(t_token *list, t_type token, char *val)
+{
+	list = ft_find_end(list);
+	list->next = ft_token_new(token, val);
+	return ;
+}
+
+void	store_token(t_token *list, char *t)
+{
+	if (ft_strncmp(t, ">>", 2))
+		ft_add_list(list, TK_DGREA, NULL);
+	else if (ft_strncmp(t, "<<", 2))
+		ft_add_list(list, TK_DLOW, NULL);
+	else if (*t == '>')
+		ft_add_list(list, TK_GREATER, NULL);
+	else if (*t == '<')
+		ft_add_list(list, TK_LOWER, NULL);
+	else if (*t == '|')
+		ft_add_list(list, TK_PIPE, NULL);
+	else if (*t == '>')
+		ft_add_list(list, TK_GREATER, NULL);
 }
 
 char	**ft_parse_split(char *line, t_token *token)
@@ -30,14 +74,21 @@ char	**ft_parse_split(char *line, t_token *token)
 	int len;
 	int i;
 	char *tmp;
+	char	s[2];
 
+	s[1] = '\0';
 	i = 0;
+	tmp = calloc(sizeof(char), 10);
 	len = ft_strlen(line);
 	while (line[i])
 	{
-
+		s[0] = line[i];
+		if (is_token(line + i) == 2)
+			i += 1;
+		tmp = ft_strjoin(tmp, s);
 		i++;
 	}
+	printf("%s", tmp);
 }
 
 int	loop(t_global *global)
@@ -54,7 +105,6 @@ int	loop(t_global *global)
 		if (read == NULL)
 			return (0);
 		add_history(read);
-		printf("%s", read);
 		if (init_parsing(read) == 1 && *read != NULL)
 		{
 			printf("Error Parsing\n");
