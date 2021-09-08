5a09b500bf5e0442ef9f816fd1852fc8f10d65a8
Lorenzo Iafigliola <liafigli@e3r6p5.42roma.it>
2021-09-08 16:33:12 +0200

    ft_is_command

--- srcs/parser.c
+++ srcs/parser.c
@@ -1,12 +1,20 @@
 #include "../includes/minishell.h"
 
+int ft_is_command(enum e_type i)
+{
+    if (i == CM_ECHO || i == CM_CD || i == CM_PWD || \
+    i == CM_EXP i == CM_UNS i == CM_ENV i == CM_EXIT)
+        return (1);
+    return (0);
+}
+
 enum e_type check_tokens_valid(t_token *token)
 {
     if (token->next)
         token = token->next;
     while (token)
     {
-        if (token->e_type != TK_ID)
+        if (token->e_type != TK_ID && !ft_is_command(token->e_type))
             if (token->e_type == TK_PIPE || token->e_type == TK_GREATER \
              || token->e_type == TK_LOWER || token->e_type == TK_DGREA || token->e_type == TK_DLOW)
                 if (token->prec->e_type != TK_ID || token->next->e_type != TK_ID)
