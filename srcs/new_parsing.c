69e371e3279f133767bcd1fa75eb163591dbbb8a
Danilo Medas <dmedas@e4r6p4.42roma.it>
2021-10-02 16:31:08 +0200

    aggiunta stati 1 2 3 4

--- srcs/new_parsing.c
+++ srcs/new_parsing.c
@@ -37,7 +37,75 @@ t_command	*ft_state_10(t_token *token)
 	}
 	else if (token->e_type == TK_PIPE)
 	{
-		return (NULL);//da cambiare
+		return (NULL);//creare un nuovo comando
+	}
+	else
+	{
+		write(1, "Parsing Error\n", 14);
+		return (NULL);
+	}
+}
+
+t_command	*ft_state_1(t_token *token)
+{
+	if (token->e_type == TK_ID)
+	{
+		return (ft_state_4(token->next));
+	}
+	else
+	{
+		write(1, "Parsing Error\n", 14);
+		return (NULL);
+	}
+}
+
+t_command	*ft_state_2(t_token *token)
+{
+	if (token->e_type == TK_ID)
+	{
+		return (ft_state_4(token->next));
+	}
+	else
+	{
+		write(1, "Parsing Error\n", 14);
+		return (NULL);
+	}
+}
+
+t_command	*ft_state_3(t_token *token)
+{
+	if (token->e_type == TK_ID)
+	{
+		return (ft_state_4(token->next));
+	}
+	else
+	{
+		write(1, "Parsing Error\n", 14);
+		return (NULL);
+	}
+}
+
+t_command	*ft_state_4(t_token *token)
+{
+	if (token->e_type == TK_PIPE)
+	{
+		return (NULL);//da creare nuovo comando
+	}
+	else if (token->e_type == TK_GREATER || token->e_type == TK_DGREA)
+	{
+		return (ft_state_1(token->next));
+	}
+	else if (token->e_type == TK_ID)
+	{
+		return (ft_state_10(token->next));
+	}
+	else if (token->e_type == TK_LOWER)
+	{
+		return (ft_state_2(token->next));
+	}
+	else if (token->e_type == TK_DLOW)
+	{
+		return (ft_state_3(token->next));
 	}
 	else
 	{
