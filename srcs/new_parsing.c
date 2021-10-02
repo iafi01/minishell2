eee4de215bda35f97180b388fd9da2371649372d
Danilo Medas <dmedas@e4r6p4.42roma.it>
2021-10-02 12:44:29 +0200

    creazione automa

---
+++ srcs/new_parsing.c
@@ -0,0 +1,39 @@
+#include "minishell.h"
+
+t_command	*ft_state_0(t_token *token)
+{
+	if (token->e_type == TK_ID)
+	{
+		return (ft_state_10(token->next));
+	}
+	else if (token->e_type == TK_GREATER || token->e_type == TK_DGREA)
+	{
+		return (ft_state_1(token->next));
+	}
+	else if (token->e_type == TK_LOWER)
+	{
+		return (ft_state_2(token->next));
+	}
+	else if (token->e_type == TK_DLOW)
+	{
+		return (ft_state_3(token->next));
+	}
+	else
+	{
+		write(1, "Parsing Error\n", 14);
+		return (NULL);
+	}
+}
+
+t_command	*ft_state_10(t_token *token)
+{
+	if (token->e_type == TK_ID)
+	{
+		return (ft_state_10(token->next));
+	}
+	else
+	{
+		write(1, "Parsing Error\n", 14);
+		return (NULL);
+	}
+}
