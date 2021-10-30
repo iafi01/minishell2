845f4a1dc84e245c085dc62d421887ae8781344c
Danilo Medas <dmedas@e4r2p1.42roma.it>
2021-10-30 19:17:23 +0200

    norminettato qualcosa ma non funziona

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -69,102 +69,103 @@ char	*ft_apici_split(char *line)
 // 	}
 // }
 
+void	str_2loop(char *line, int *j, char *tmp, char *s)
+{
+	char	*str;
+	while (line[++j[1]])
+	{
+		j[0]++;
+		if (line[j[1]] == 0 || line[j[1]] == ' ')
+			break ; 
+		else if (line[j[1]] == 39)
+		{
+			str = ft_stringa_unica(line + j[1], j, 1);
+			tmp = ft_strjoin(tmp, str);
+			j[1] += ft_strlen(str);
+			break ;
+		}
+		else if (line[j[1]] == 34)
+		{
+			str = ft_stringa_unica(line + j[1], j, 2);
+			tmp = ft_strjoin(tmp, str);
+			j[1] += ft_strlen(str);
+			break ;
+		}
+		s[0] = line[j[1]];
+		tmp = ft_strjoin(tmp, s);
+	}
+}
+
 char *ft_stringa_unica(char *line, int *j, int ap)
 {
-	int i;
-	char *tmp;
-	char	*str;
-	char s[2];
+	char	*tmp;
+	char	s[2];
 
 	s[1] = '\0';
 	tmp = calloc(30, sizeof(char));
-	i = 0;
-	while (line[++i])
+	j[1] = 0;
+	while (line[++j[1]])
 	{
 		j[0]++;
-		if ((line[i] == 39 && ap == 1) || (line[i] == 34 && ap == 2))
-		{
+		if ((line[j[1]] == 39 && ap == 1) || (line[j[1]] == 34 && ap == 2))
 			break ;
-		}
-		s[0] = line[i];
+		s[0] = line[j[1]];
 		tmp = ft_strjoin(tmp, s);
 	}
-	if (line[i + 1] && line[i + 1] != ' ')
+	if (line[j[1] + 1] && line[j[1] + 1] != ' ')
 	{
-		while (line[++i])
-		{
-			j[0]++;
-			if (line[i] == 0 || line[i] == ' ')
-				break ; 
-			else if (line[i] == 39)
-			{
-				str = ft_stringa_unica(line + i, j, 1);
-				tmp = ft_strjoin(tmp, str);
-				i += ft_strlen(str);
-				break ;
-			}
-			else if (line[i] == 34)
-			{
-				str = ft_stringa_unica(line + i, j, 2);
-				tmp = ft_strjoin(tmp, str);
-				i += ft_strlen(str);
-				break ;
-			}
-			s[0] = line[i];
-			tmp = ft_strjoin(tmp, s);
-		}
+		str_2loop(line, j, tmp, s);
 	}
-	//printf("tmp%s\n",tmp);
-	if (line[i] == 34)
-		tmp = ft_strjoin(tmp, ft_stringa_unica(line + i, j, 2));
-	else if (line[i] == 39)
-		tmp = ft_strjoin(tmp, ft_stringa_unica(line + i + 1, j, 1));
+	if (line[j[1]] == 34)
+		tmp = ft_strjoin(tmp, ft_stringa_unica(line + j[1], j, 2));
+	else if (line[j[1]] == 39)
+		tmp = ft_strjoin(tmp, ft_stringa_unica(line + j[1] + 1, j, 1));
 	return (tmp);
 }
 
 void	ft_lexer(char *line, t_token *token)
 {
 	int len;
-	int i;
+	int i[2];
 	char *tmp;
 	char	s[2];
 	
 	s[1] = '\0';
-	i = -1;
+	i[0] = -1;
 	tmp = malloc(sizeof(char) * 30); //cambiare 30
 	len = ft_strlen(line);
-	while (line[++i])
+	while (line[++i[0]])
 	{
-		if (line[i] == 34 || line[i] == 39)
+		if (line[*i] == 34 || line[*i] == 39)
 		{
-			if (line[i] == 34)
-				ft_add_list(token, TK_ID, ft_stringa_unica(line + i, &i, 2), 2);
-			else if (line[i] == 39)
-				ft_add_list(token, TK_ID, ft_stringa_unica(line + i, &i, 1), 1);
+			if (line[*i] == 34)
+				ft_add_list(token, TK_ID, ft_stringa_unica(line + *i, i, 2), 2);
+			else if (line[*i] == 39)
+				ft_add_list(token, TK_ID, ft_stringa_unica(line + *i, i, 1), 1);
 			continue ;
 		}
-		if (line[i] == 34 || line[i] == 39)
+		if (line[*i] == 34 || line[*i] == 39)
 		{
-			ft_strjoin(tmp, ft_apici_split(line + i));
+			ft_strjoin(tmp, ft_apici_split(line + *i));
 			continue ;
 		}
-		if (is_token(line + i) == 0 && line[i] != 32)
+		if (is_token(line + *i) == 0 && line[*i] != 32)
 		{
-			s[0] = line[i];
+			s[0] = line[*i];
 			tmp = ft_strjoin(tmp, s);
 			s[0] = '\0';
 		}
-		if (*tmp == (char)NULL && line[i] == 32)
+		if (*tmp == (char)NULL && line[*i] == 32)
 			continue;
-		if (line[i] && (is_token(line + i) || line[i] == 32))
+		if (line[*i] && (is_token(line + *i) || line[*i] == 32))
 		{
 			//printf(":%c", line[i]);
-			if (is_token(line + i))
+			if (is_token(line + *i))
 			{
 				//printf("&%c&", line[i]);
-				store_token(token, line + i);
-				if (is_token(line + i) == 2)
-					i++;
+				store_token(token, line + *i);
+				if (is_token(line + *i) == 2)
+					(*i)++;
 				continue;
 			}
 			else if (tmp)
@@ -178,32 +179,51 @@ void	ft_lexer(char *line, t_token *token)
 			}
 		}
 	}
-	if (line[i] == '\0' && tmp && *tmp != (char)NULL && line[i] != 32)
+	if (line[*i] == '\0' && tmp && *tmp != (char)NULL && line[*i] != 32)
 		ft_add_list(token, TK_ID, tmp, 0);
 		// printf("&%s&\n", tmp);
 }
 
+void	loop_core(t_global *global, char *read)
+{
+	ft_lexer(read, global->token);
+	global->simple_cmd = ft_command_new();
+	global->simple_cmd = ft_cmd_init(global->simple_cmd);
+	ft_state_0(global->token->next, global->simple_cmd);
+	ft_exe_commands(global, global->simple_cmd);
+	ft_free_list(global->token);
+	free(read);
+}
+
+char	*pre_loop(void)
+{
+	char	*user;
+	char	*read;
+
+	wait(0);
+	signal(SIGINT, sig_handler);
+	signal(SIGQUIT, sig_handler);
+	read = ft_strjoin("<", getenv("USER"));
+	user = ft_strjoin(read, "> ");
+	free(read);
+	read = readline(user);
+	free(user);
+	if (read == NULL)
+	{
+		printf("exit");
+		exit(0);
+	}
+	return (read);
+}
+
 int	loop(t_global *global)
 {
-	char		*read;
-	char		*user;
-	int	f;
+	char	*read;
+	int		f;
 
 	while (1)
 	{
-		wait(0);
-		signal(SIGINT, sig_handler);
-		signal(SIGQUIT, sig_handler);
-		read = ft_strjoin("<", getenv("USER"));
-		user = ft_strjoin(read, "> ");
-		free(read);
-		read = readline(user);
-		free(user);
-		if (read == NULL)
-		{
-			printf("exit");
-			exit(0);
-		}
+		read = pre_loop();
 		if (!strncmp(read, "", 2))
 			continue;
 		add_history(read);
@@ -214,21 +234,14 @@ int	loop(t_global *global)
 			free(read);
 			continue ;
 		}
-		ft_lexer(read, global->token);
-		// debug_list(global->token);
-		global->simple_cmd = ft_command_new();
-		global->simple_cmd = ft_cmd_init(global->simple_cmd);
-		ft_state_0(global->token->next, global->simple_cmd);
-		ft_exe_commands(global, global->simple_cmd);
-		ft_free_list(global->token);
-		free(read);
+		loop_core(global, read);
 	}
 	return (1);
 }
 
 int	main(int argc, char **argv, char **envp)
 {
-	t_token		*token;
+	t_token	*token;
 
 	glbl.pid = 0;
 	token = ft_token_new(TK_ID, NULL, 0, NULL);
@@ -238,7 +251,6 @@ int	main(int argc, char **argv, char **envp)
 	glbl.token = token;
 	glbl.envp = ft_copy_env(envp);
 	glbl.size = ft_get_size(envp);
-
 	if (!loop(&glbl))
 		return (0);
 }
